be1164fc853c6501b2287b9ed35e2e8ab0288fe6
BYUFootball <bensauer+bryant@gmail.com>
2016-08-26 18:01:21 -0700

    Update activity_picker.py

--- activity_picker.py
+++ activity_picker.py
@@ -1,8 +1,13 @@
 # import special libraries already built in python
-import random
+import random, urllib2
 
 # list of options to select from
-possible_activities = ['a', 'b', 'c']
+the_url = 'https://raw.githubusercontent.com/BYUFootball/volcanion/62ee27e26791cb57b28e6cfb0727489c70d6c4a2/activities.fe'
+list_raw_text = urllib2.urlopen(the_url).read()
+
+print "DEBUG: " + str(list_raw_text.split())
+
+possible_activities = list_raw_text.split()
 
 # choice of what we are going to do
 the_activity = random.choice(possible_activities)
@@ -10,3 +15,19 @@ the_activity = random.choice(possible_activities)
 # display the results to the end user
 print "Possible activities are: " + str(possible_activities)
 print "What we'll do today: " +  the_activity
+# import special libraries already built in python
+
+
+
+
+
+
+
+# choice of what we are going to do
+the_other_activity = random.choice(possible_activities)
+while the_activity != the_other_activity:
+    print "Repicking"
+    the_other_activity = random.choice(possible_activities)
+
+# display the results to the end user
+print "What we'll do today: " +  the_other_activity
